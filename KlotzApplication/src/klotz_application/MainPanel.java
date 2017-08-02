5788d4648b4025193fc79d1ce26bb459cf78fa9c
Hasan Karahan <hasan.karahan@blackhan.com>
2017-08-02 17:04:33 +0300

    refactored code base

--- KlotzApplication/src/klotz_application/MainPanel.java
+++ KlotzApplication/src/klotz_application/MainPanel.java
@@ -8,58 +8,20 @@ import java.awt.Graphics;
  */
 public class MainPanel extends javax.swing.JPanel {
 
-    private Integer i = null;
-    private final Integer di = 48;
-    private Integer j = null;
-    private final Integer dj = 48;
+    private Integer center_x = null;
+    private final Integer center_dx = 48;
+    private Integer center_y = null;
+    private final Integer center_dy = 48;
 
-    private Integer x = null;
-    private final Integer dx = 48;
-    private Integer y = null;
-    private final Integer dy = 48;
+    private Integer moving_x = null;
+    private final Integer moving_dx = 48;
+    private Integer moving_y = null;
+    private final Integer moving_dy = 48;
 
-    /**
-     * Creates new form MainPanel
-     */
     public MainPanel() {
         initComponents();
     }
 
-    @Override
-    public void paintComponent(Graphics g) {
-
-        if (this.i == null) {
-            this.i = this.getWidth()/2;
-        }
-        if (this.j == null) {
-            this.j = this.getHeight() - this.dj/2 - 1;
-        }
-
-        if (this.x == null) {
-            this.x = this.getWidth() - this.dx/2 - 1;
-        }
-        if (this.y == null) {
-            this.y = this.getHeight() - this.dy/2 - 1;
-        }
-
-        g.setColor(Color.black);
-        g.fillRect(0, 0, this.getWidth(), this.getHeight());
-
-        g.setColor(this.j - this.y == 0 
-                && this.x > this.i 
-                && this.x < this.i + this.di ? Color.red : Color.yellow);
-        g.fillRect(this.i-this.di/2, this.j-this.dj/2, this.di, this.dj);
-
-        g.setColor(Color.cyan);
-        g.fillRect(this.x-this.dx/2, this.y-this.dy/2, this.dx, this.dy);
-
-        if (this.x + this.dx > 0) {
-            this.x = (this.x - 1) % this.getWidth();
-        } else {
-            this.x = this.getWidth();
-        }
-    }
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -86,14 +48,78 @@ public class MainPanel extends javax.swing.JPanel {
         this.repaint();
     }//GEN-LAST:event_formMouseMoved
 
-    public void jump() {
-        if (this.j == this.dj/2 + 1) {
-            this.j = this.getHeight() - this.dj/2 - 1;
+    @Override
+    public void paintComponent(Graphics g) {
+
+        this.calculateCenterBlock();
+        this.calculateMovingBlock();
+
+        this.renderGameField(g);
+        this.renderCenterBlock(g);
+        this.renderMovingBlock(g);
+    }
+
+    private void calculateCenterBlock() {
+        if (this.center_x == null) {
+            this.center_x = this.getWidth()/2;
+        }
+        if (this.center_y == null) {
+            this.center_y = this.getHeight() - this.center_dy/2 - 1;
+        }
+    }
+
+    private void calculateMovingBlock() {
+        if (this.moving_x == null) {
+            this.moving_x = this.getWidth() - this.moving_dx/2 - 1;
+        }
+        if (this.moving_y == null) {
+            this.moving_y = this.getHeight() - this.moving_dy/2 - 1;
+        }
+        if (this.moving_x + this.moving_dx > 0) {
+            this.moving_x = (this.moving_x - 1) % this.getWidth();
         } else {
-            this.j = this.dj/2 + 1;
+            this.moving_x = this.getWidth();
         }
     }
     
+    private void renderGameField(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+    }
+
+    private void renderCenterBlock(Graphics g) {
+        if (this.center_y - this.moving_y == 0
+         && this.moving_x > this.center_x
+         && this.moving_x < this.center_x + this.center_dx)
+        {
+            g.setColor(Color.red);
+        } else {
+            g.setColor(Color.yellow);
+        }
+        g.fillRect(
+                this.center_x - this.center_dx / 2,
+                this.center_y - this.center_dy / 2,
+                this.center_dx,
+                this.center_dy);
+    }
+
+    private void renderMovingBlock(Graphics g) {
+        g.setColor(Color.cyan);
+        g.fillRect(
+                this.moving_x - this.moving_dx / 2,
+                this.moving_y - this.moving_dy / 2,
+                this.moving_dx,
+                this.moving_dy);
+    }
+
+    public void toggleCenterBlock() {
+        if (this.center_y == this.center_dy/2 + 1) {
+            this.center_y = this.getHeight() - this.center_dy/2 - 1;
+        } else {
+            this.center_y = this.center_dy/2 + 1;
+        }
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
 }
